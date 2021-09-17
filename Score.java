d29112065bf11701b3a3963d441a91e74174454f
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2021-09-17 21:44:53 +0200

    Create Score.java

---
+++ Score.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Score extends Rectangle{
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+    public Score(int GAME_WIDTH, int GAME_HEIGHT) {
+    }
+
+    public void draw(Graphics g) {
+
+    }
+}
